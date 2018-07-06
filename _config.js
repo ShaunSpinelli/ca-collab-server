e936664a4ea9f49ba58e54140cdb6f2437992d51
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-07-06 11:31:58 +1000

    added test user name and passwrod to env

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.mongoURI = {
   development: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds125871.mlab.com:25871/cs-collab`,
-  test: 'mongodb://dbadmin:dbadmin1@ds127841.mlab.com:27841/ca-collab-test'
+  test: `mongodb://${process.env.DB_USER_TEST}:${process.env.DB_PASS_TEST}@ds127841.mlab.com:27841/ca-collab-test`
 };
 
 module.exports = config;
