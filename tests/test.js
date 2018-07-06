7ba68f3700f789ed78948e3d25fee5b3d4939475
Shaun Spinelli <shaun.spinelli@gmail.com>
2018-07-06 11:04:33 +1000

    updated tests to work with remote testing db and to clear db after tests

--- tests/test.js
+++ tests/test.js
@@ -1,36 +1,47 @@
-const app = require('./app')
+process.env.NODE_ENV = 'test';
+const app = require('../index')
 //chai tests
 const chai = require('chai')
 const should = chai.should()
 const chaiHttp = require('chai-http')
+const mongoose = require("mongoose");
 
 chai.use(chaiHttp)
 
+const BlogPost = require('../models/BlogPost')
+const User = require('../models/User')
+const Topic = require('../models/Topic')
 
 
-describe('POST /topics', () => {
-    // this.timeout(15000)
-    it('Adds topic to database', (done) => {
-        chai.request(app)
-            .post('/topics')
-            .send({
-                "title": "book",
-                "parentId": "1000"
-            })
-            .end((err, res) => {
-                should.equal(err, null)
-                res.should.be.json;
-                res.body.should.be.a('object')
-                res.body.should.have.property('title')
-                res.body.should.have.property('parentId')
-                res.should.have.status(200)
-                done()
-            })
+
+    describe('POST /topics', function () {
+        this.timeout(15000)
+        
+        it('Adds topic to database', (done) => {
+            chai.request(app)
+                .post('/topics')
+                .send({
+                    "title": "book",
+                    "parentId": "1000"
+                })
+                .end((err, res) => {
+                    should.equal(err, null)
+                    res.should.have.status(200) 
+                    res.should.be.json;
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('title')
+                    res.body.should.have.property('parentId')
+                    done()
+                })
+        })
     })
-})
 
-describe('GET /Topics', () => {
-    // this.timeout(15000)
+describe('GET /Topics', function () {
+    this.timeout(15000)
+    afterEach(function(done){
+            Topic.collection.drop();
+            done();
+        })
     it('should return all our Topics', (done) => {
         chai.request(app)
             .get('/Topics')
@@ -45,11 +56,12 @@ describe('GET /Topics', () => {
 })
 
 
-describe('POST /blogposts', () => {
-    // this.timeout(15000)
+describe('POST /blogpost', function () {
+    this.timeout(15000)
+    
     it('add topic to database', (done) => {
         chai.request(app)
-            .post('/blogposts')
+            .post('/blogpost')
             .send({
                 title: "ben the worm",
                 content: "this is a story a bout a worm named ben",
@@ -70,11 +82,15 @@ describe('POST /blogposts', () => {
     })
 })
 
-describe('GET /Blogposts', () => {
-    // this.timeout(15000)
+describe('GET /Blogpost', function () {
+    this.timeout(15000)
+    afterEach(function(done){
+        BlogPost.collection.drop();
+        done();
+    })
     it('should return all our Blog Posts', (done) => {
         chai.request(app)
-            .get('/blogposts')
+            .get('/blogpost')
             .end((err, res) => {
                 should.equal(err, null)
                 res.body.should.be.a('array')
@@ -88,12 +104,17 @@ describe('GET /Blogposts', () => {
     })
 })
 
-describe('POST /user/register',()=>{
+describe('POST /user/register',function (){
+    this.timeout(15000)
+    beforeEach(function(done){
+        User.collection.drop();
+        done();
+      })
     it('Should register a new user', (done)=>{
         chai.request(app)
             .post('/user/register')
             .send({
-                email: 'jimdmy@jimmail.com',
+                email: 'jimdmy@jiimmail.com',
                 password: 'pass',
                 role: 'teacher'
             })
@@ -101,8 +122,8 @@ describe('POST /user/register',()=>{
                 should.equal(err, null)
                 res.should.have.status(200)
                 res.body.should.be.a('object')
-                res.body.should.have.property('email')
-                res.body.should.have.property('role')
+                res.body.should.have.property('token')
+                // res.body.should.have.property('role')
                 // res.body.role.should.be('teacher')
                 done()
             })
